edfa617058e25d52a89b22cc31a600df584aa649
William Eastbury <William.eastbury@microsoft.com>
2023-07-31 17:50:27 +0100

    Update buildandpush.cmd

--- buildandpush.cmd
+++ buildandpush.cmd
@@ -1,6 +1,12 @@
 # Create the ARM Group (and the ACR and app service instances) 
 az deployment group create --resource-group appframe --template-file ./acr.bicep --parameters acrName=appframeukwacr
 
+# Get the admin username
+az acr credential show --name appframeukwacr --query "username"
+
+# Get the admin password
+az acr credential show --nameappframeukwacr --query "passwords[0].value"
+
 # Login to the ACR
 az acr login --name appframeukwacr
 cd /AppFrame/Services/TableServiceAPI
