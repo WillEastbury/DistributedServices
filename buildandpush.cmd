509bc9bcc3aac03a95496951831af9f8d93006ce
William Eastbury <William.eastbury@microsoft.com>
2023-07-31 18:05:23 +0100

    Update buildandpush.cmd

--- buildandpush.cmd
+++ buildandpush.cmd
@@ -1,11 +1,11 @@
 # Create the ARM Group (and the ACR and app service instances) 
-az deployment group create --resource-group appframe --template-file ./acr.bicep --parameters acrName=appframeukwacr
+az deployment group create --resource-group appframe --template-file ./acr.bicep --parameters acrName=appframeukwacr --query properties.outputs.acrLoginServer.value
 
 # Get the admin username
 az acr credential show --name appframeukwacr --query "username"
 
 # Get the admin password
-az acr credential show --nameappframeukwacr --query "passwords[0].value"
+az acr credential show --name appframeukwacr --query "passwords[0].value"
 
 # Login to the ACR
 az acr login --name appframeukwacr
